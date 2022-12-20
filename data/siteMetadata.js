3d81291edef0d0bcb1cb7333df90b40bcfb37662
Marcin <marcin@pucilowski.com>
2022-12-20 21:41:59 +0000

    pudingovsky.com

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -2,9 +2,9 @@
 
 /** @type {import("pliny/config").PlinyConfig } */
 const siteMetadata = {
-  title: 'Next.js Starter Blog',
-  author: 'Tails Azimuth',
-  headerTitle: 'TailwindBlog',
+  title: 'Pudingovsky Notes',
+  author: 'Pavel Pudingovsky',
+  headerTitle: 'PUDINGOVSKY, Pavel',
   description: 'A blog created with Next.js and Tailwind.css',
   language: 'en-us',
   theme: 'system', // system, dark or light
